81943fd22a95b4e8ba2a42da2da1f272cdc72d43
Bajlar01~ <bajlarrahman24@gmail.com>
2023-08-09 16:25:23 +0600

    responsive website jhaka naka production

---
+++ js/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#FECC02",
+        white: "#FFFFFF",
+        black: "#131313",
+        grey: "#4E4E4E",
+      },
+      fontFamily: {
+        primary: ["Bebas Neue", "sans-serif"],
+        secondary: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+};
